bace9823551afe577414fb0e150ce30419d45c7c
Miroslav Petrov <miroslav_petrov@outlook.com>
2014-05-22 23:24:14 +0300

    Add Java-Syntax-Homework

---
+++ JavaBasics/Java-Syntax-Homework/src/DecimalToHexadecimal.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class DecimalToHexadecimal {
+
+	public static void main(String[] args){
+		
+		Scanner reader = new Scanner(System.in);
+		while (true) {
+			System.out.print("Enter number: ");
+			int number = reader.nextInt();
+			String hex = Integer.toHexString(number).toUpperCase();
+			System.out.print("Hex: ");
+			System.out.println(hex);
+			System.out.println("=====================");
+		}
+	}
+}
