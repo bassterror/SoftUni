ac3fed19aee1d9d4dd09afa7b55f416df1f0249c
Miroslav Petrov <miroslav_petrov@outlook.com>
2015-08-03 22:31:26 +0300

    Database applications

---
+++ Database applications/EntityFramework/Entity-Framework-Homework/EmployeeDAOClass/EmployeeDAOClass.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeDAOClass
+{
+    public class EmployeeDAOClass
+    {
+        public void Add(Employees employee)
+        {
+            using (SoftUniEntities entities = new SoftUniEntities())
+            {
+                entities.Employees.Add(employee);
+                entities.SaveChanges();
+            }
+        }
+
+        public Employees FindByKey(object key)
+        {
+            using (SoftUniEntities entities = new SoftUniEntities())
+            {
+                Employees employee = entities.Employees.Find(key);
+                return employee;
+            }
+        }
+    }
+}
---
+++ Database applications/EntityFramework/Entity-Framework-Homework/EmployeeDAOClass/Test.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeDAOClass
+{
+    class Test
+    {
+        static void Main(string[] args)
+        {
+            EmployeeDAOClass daoClass = new EmployeeDAOClass();
+            using (SoftUniEntities entities = new SoftUniEntities())
+            {
+                Employees employee = new Employees()
+                {
+                    FirstName = "Zahari",
+                    LastName = "Zahariev",
+                    MiddleName = "Z",
+                    JobTitle = "Production Technician",
+                    DepartmentID = 7,
+                    ManagerID = 16,
+                    HireDate = DateTime.Now,
+                    Salary = 125000.0M,
+                    AddressID = 102
+                };
+                daoClass.Add(employee);
+                Employees newEmployee = entities.Employees.FirstOrDefault(e => e.FirstName == "Zahari");
+                Console.WriteLine(newEmployee.EmployeeID);
+                
+            }
+        }
+    }
+}
